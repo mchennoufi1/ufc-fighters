2c2b9a9b7bb958da24718573a51027598034bac6
Mahmoud Issa <302612351@student.rocmondriaan.nl>
2023-01-12 09:39:11 +0100

    PEEPEEPOOPOO

--- Templates/defaults/menu.php
+++ Templates/defaults/menu.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="/home">
-            Sportcenter
+            Fighters
         </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
 
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="navbar-nav">
 
                 <li class="nav-item">
-                    <a class="nav-link" href="/categories">sportapparaat</a>
+                    <a class="nav-link" href="/categories">Rankings</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link"  href="/register">registreren</a>
